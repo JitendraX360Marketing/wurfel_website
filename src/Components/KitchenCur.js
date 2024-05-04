9c4688c2b9c95e49da80e08a89eea252c70b5ce1
JitendraX360Marketing <162128098+JitendraX360Marketing@users.noreply.github.com>
2024-05-04 14:31:02 +0530

    commit

--- src/Components/KitchenCur.js
+++ src/Components/KitchenCur.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./kitchenCur.css";
+import curItem1 from "../ImagesCsl/kitchen_01.jpg";
+import curItem2 from "../ImagesCsl/kitchen_02.jpg";
+import curItem3 from "../ImagesCsl/kitchen_03.jpg";
 
 export default function KitchenCur() {
   return (
     <>
       <section className="KitchenCurSection">
+        <span>Left</span>
         <div className="mainDivision_KitchenCurSection">
-    
+          <div className="curItem1">
+          </div>
+          <div className="curItem2">
+          </div>
+          <div className="curItem3">
+          </div>
         </div>
+        <span>Right</span>
       </section>
     </>
   );
